d876ed73bb26e20d88ae6f4e8b087177827d78f1
Oways Jaffer <realowaysjaffer@gmail.com>
2023-07-23 15:47:46 -0400

    test

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import utils from "./helpers/utils.js";
 import path from "path";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 global.path = path;
 global.dotenv = dotenv;
@@ -14,7 +15,10 @@ app.use(express.static(path.resolve(process.cwd(), 'public')))
 app.get('/api', (req, res) => {
     res.send('Express to the rescue!');
 });
+//express router and /user
 
+//keep at very end
 app.listen(process.env.port, () => {
     utils.log(`Server has started and is listening on port ${process.env.port}!`)
 });
+
